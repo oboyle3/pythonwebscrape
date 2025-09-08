828e5164704321419b7eeef503c9a992bf6f501b
oboyle3 <oboyle3patrick@gmail.com>
2025-09-07 22:32:28 -0400

    countries scrape

---
+++ home/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('countries/', views.scrape_countries, name='countries'),
+]
---
+++ home/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from bs4 import BeautifulSoup
+import requests
+# Create your views here.
+def index(request):
+    return render(request, 'home/index.html')
+
+def scrape_countries(request):
+    url = "https://www.scrapethissite.com/pages/simple/"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    #Extract Countries
+    countries = []
+    for country in soup.find_all("div",class_="country"):
+        name = country.find("h3",class_="country-name").get_text(strip=True)
+
+        countries.append({
+            "name": name
+        })
+    return render(request,"home/countries.html", {"countries":countries})
+
